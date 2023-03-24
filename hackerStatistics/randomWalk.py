d7be1b7e0e3966db7dc7741987958bbfab44b8b2
AmitGit217 <amit217@yandex.com>
2023-03-24 09:29:03 +0300

    feat: create histogram of distribution

--- hackerStatistics/randomWalk.py
+++ hackerStatistics/randomWalk.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Simulate random walk 250 times
+# Simulate random walk 500 times
 all_walks = []
-for i in range(250):
+for i in range(500):
     random_walk = [0]
     for x in range(100):
         step = random_walk[-1]
@@ -15,13 +15,12 @@ for i in range(250):
             step = step + 1
         else:
             step = step + np.random.randint(1, 7)
-
         if np.random.rand() <= 0.001:
             step = 0
-
         random_walk.append(step)
     all_walks.append(random_walk)
 
 np_aw_t = np.transpose(np.array(all_walks))
-plt.plot(np_aw_t)
+ends = np_aw_t[-1, :]
+plt.hist(ends)
 plt.show()
