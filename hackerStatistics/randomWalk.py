ccf7831b8f5b15f09415e5f65b4dea33192724be
AmitGit217 <amit217@yandex.com>
2023-03-24 09:25:34 +0300

    feat: add a chance to fall

--- hackerStatistics/randomWalk.py
+++ hackerStatistics/randomWalk.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# Simulate random walk 250 times
 all_walks = []
-for i in range(10):
+for i in range(250):
     random_walk = [0]
     for x in range(100):
         step = random_walk[-1]
@@ -14,11 +15,13 @@ for i in range(10):
             step = step + 1
         else:
             step = step + np.random.randint(1, 7)
+
+        if np.random.rand() <= 0.001:
+            step = 0
+
         random_walk.append(step)
     all_walks.append(random_walk)
 
-
-np_aw = np.array(all_walks)
-np_aw_t = np.transpose(np_aw)
+np_aw_t = np.transpose(np.array(all_walks))
 plt.plot(np_aw_t)
 plt.show()
