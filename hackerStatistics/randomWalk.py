29048e633f73f3003abc84de1dbaa075e70a519c
AmitGit217 <amit217@yandex.com>
2023-03-24 09:05:36 +0300

    fix: cleaner code

--- hackerStatistics/randomWalk.py
+++ hackerStatistics/randomWalk.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Initialize random_walk
-random_walk = [0]
 
+random_walk = [0]
 for x in range(100):
+
     step = random_walk[-1]
     dice = np.random.randint(1, 7)
 
     if dice <= 2:
-        # Replace below: use max to make sure step can't go below 0
         step = max(0, step - 1)
     elif dice <= 5:
         step = step + 1
@@ -21,9 +20,5 @@ for x in range(100):
 print(random_walk)
 
 
-# Plot random_walk
 plt.plot(random_walk)
-
-
-# Show the plot
 plt.show()
