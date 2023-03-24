8508dc5106a9f37b94b5903f56855debe34d3081
AmitGit217 <amit217@yandex.com>
2023-03-24 08:55:19 +0300

    feat: random walk

--- hackerStatistics/diceRoll.py
+++ hackerStatistics/diceRoll.py
@@ -1,13 +1,6 @@
 import numpy as np
 
 np.random.seed(123)
-# Random with no context
-print(np.random.rand())
-
-# Random with context
-print(np.random.randint(1, 7))
-print(np.random.randint(1, 7))
-
 # Starting step
 step = 50
 
