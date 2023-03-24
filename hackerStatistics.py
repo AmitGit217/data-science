339a7d4129ccda8444af7fb4ed0a22870de46495
AmitGit217 <amit217@yandex.com>
2023-03-24 08:38:06 +0300

    feat: rolling a dice and initialzing the game

--- hackerStatistics.py
+++ hackerStatistics.py
@@ -7,3 +7,21 @@ print(np.random.rand())
 # Random with context
 print(np.random.randint(1, 7))
 print(np.random.randint(1, 7))
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1, 7)
+
+# Finish the control construct
+if dice <= 2:
+    step = step - 1
+elif dice <= 5:
+    step = step + 1
+else:
+    step = step + np.random.randint(1, 7)
+
+# Print out dice and step
+print(step)
+print(dice)
